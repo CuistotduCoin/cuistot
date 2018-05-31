919526ec48275c78380a9203f7ec26df80e12b4e
Romain Quellec <romain.quellec@gmail.com>
2018-05-31 14:58:01 +0200

    Serverless deployment

--- frontend/razzle.config.js
+++ frontend/razzle.config.js
@@ -1,14 +1,11 @@
-'use strict';
-
 module.exports = {
-
   plugins: ['typescript'],
 
-  modify(config, { target, dev }) {
-    
+  modify(config, { target, dev }, webpack) {
+    const appConfig = config // stay immutable here
     if (target === 'node' && !dev) {
-      config.externals = [];
+      appConfig.externals = [];
     }
-    return config;
+    return appConfig;
   }
 };
