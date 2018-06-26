a1db2af1cbbfbb8a64213894b293e51353cfdab0
Romain Quellec <romain.quellec@gmail.com>
2018-06-26 10:49:55 +0200

    Classname Sync

--- frontend/src/client.tsx
+++ frontend/src/client.tsx
@@ -1,8 +1,15 @@
 import { After, ensureReady } from "@jaredpalmer/after";
-import { MuiThemeProvider } from "@material-ui/core/styles";
+import {
+  createGenerateClassName,
+  MuiThemeProvider
+} from "@material-ui/core/styles";
 import * as React from "react";
 import { ApolloProvider } from "react-apollo";
 import { hydrate } from "react-dom";
+// @ts-ignore
+import { SheetsRegistry } from "react-jss/lib/jss";
+// @ts-ignore
+import JssProvider from "react-jss/lib/JssProvider";
 import { BrowserRouter } from "react-router-dom";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
@@ -14,14 +21,23 @@ import awsconfig from "./shared/auth";
 import theme from "./theme";
 
 const client = createApolloClient({ ssrMode: false });
+const sheetsRegistry = new SheetsRegistry();
+const generateClassName = createGenerateClassName({
+  productionPrefix: "c"
+});
 
 ensureReady(routes).then(data =>
   hydrate(
     <ApolloProvider client={client}>
       <BrowserRouter>
-        <MuiThemeProvider theme={theme}>
-          <After data={data} routes={routes} />
-        </MuiThemeProvider>
+        <JssProvider
+          registry={sheetsRegistry}
+          generateClassName={generateClassName}
+        >
+          <MuiThemeProvider theme={theme}>
+            <After data={data} routes={routes} />
+          </MuiThemeProvider>
+        </JssProvider>
       </BrowserRouter>
     </ApolloProvider>,
     document.getElementById("root"),
