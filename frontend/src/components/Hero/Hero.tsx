c88e5d47af45d8367c50d502b70c90c71855ef5c
Romain Quellec <romain.quellec@gmail.com>
2018-07-02 16:59:57 +0200

    Change Hero component

--- frontend/src/components/Hero/Hero.tsx
+++ frontend/src/components/Hero/Hero.tsx
@@ -2,48 +2,39 @@ import Grid from "@material-ui/core/Grid";
 import { Theme, withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 
 const styles = (theme: Theme) => ({
-  background: {
-    backgroundPosition: "50%",
-    backgroundSize: "cover",
-    bottom: 0,
-    left: 0,
+  content: {
+    alignItems: "center",
+    background: "rgba(0, 0, 0, 0.7)",
+    display: "flex"
+  },
+  grid: {
+    margin: "0px auto",
+    maxWidth: 1080,
+    padding: 24
+  },
+  home: {
+    color: "#fff",
     overflow: "hidden",
+    position: "relative"
+  },
+  video: {
+    bottom: 0,
+    left: "50%",
+    minHeight: "100%",
+    minWidth: "100%",
     position: "absolute",
     right: 0,
     top: 0,
-    width: "100%"
-  },
-  content: {
-    backgroundColor: "transparent",
-    left: 0,
-    position: "absolute",
-    right: 0,
-    top: "120px",
-    zIndex: 1
-  },
-  hero: {
-    bottom: "0%",
-    height: "100%",
-    left: "0%",
-    overflow: "hidden",
-    position: "relative",
-    width: "100%"
-  },
-  home: {
-    height: "300px"
-  },
-  video: {
-    opacity: 1,
-    position: "relative",
-    width: "100%"
+    transform: "translateX(-50%)",
+    zIndex: -1
   }
 });
 
 interface IHeroProps {
   classes?: any;
+  height: number;
   videoURL: string;
   imageURL: string;
   valueProposition: string;
@@ -51,10 +42,16 @@ interface IHeroProps {
 }
 
 export class Hero extends Component<IHeroProps, {}> {
-  public static defaultProps = { valueProposition: "Des saveurs à partager" };
+  public static defaultProps: Partial<IHeroProps> = {
+    height: 500,
+    valueProposition: "Des saveurs à partager"
+  };
 
-  private innerStyle = {
+  private innerBackground = {
     backgroundImage: `url(${this.props.imageURL})`
+  };
+  private innerHeight = {
+    height: this.props.height
   };
 
   public render() {
@@ -62,32 +59,41 @@ export class Hero extends Component<IHeroProps, {}> {
 
     return (
       <div className={classes.home}>
-        <div className={classes.hero}>
-          <div className={classes.background} style={this.innerStyle} />
-          <video
-            className={classes.video}
-            autoPlay={true}
-            muted={true}
-            loop={true}
-            poster={this.props.imageURL}
-          >
-            <source src={this.props.videoURL} type="video/mp4" />
-          </video>
-          <div className={classes.content}>
-            <Grid container={true} justify="space-around">
-              <Grid item={true} xs={12}>
-                <Typography variant="title" align="center" component="h1">
-                  {this.props.valueProposition}
+        <div className={classes.content} style={this.innerHeight}>
+          <Grid container={true} direction="column">
+            <Grid item={true}>
+              <Typography
+                variant="title"
+                align="center"
+                component="h1"
+                color="inherit"
+              >
+                {this.props.valueProposition}
+              </Typography>
+              {this.props.description && (
+                <Typography
+                  variant="subheading"
+                  align="center"
+                  component="p"
+                  color="inherit"
+                >
+                  {this.props.description}
                 </Typography>
-                {this.props.description && (
-                  <Typography variant="subheading" align="center" component="p">
-                    {this.props.description}
-                  </Typography>
-                )}
-              </Grid>
+              )}
             </Grid>
-          </div>
+          </Grid>
         </div>
+
+        <div className={classes.video} style={this.innerBackground} />
+        <video
+          className={classes.video}
+          autoPlay={true}
+          muted={true}
+          loop={true}
+          poster={this.props.imageURL}
+        >
+          <source src={this.props.videoURL} type="video/mp4" />
+        </video>
       </div>
     );
   }
