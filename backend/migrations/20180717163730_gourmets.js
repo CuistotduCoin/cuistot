03f5d34cdb77456d78045c2f6fcf6526f5acaad9
stephme <stephane@inchbase.com>
2018-07-18 15:03:07 +0200

    Fix indentation

--- backend/migrations/20180717163730_gourmets.js
+++ backend/migrations/20180717163730_gourmets.js
@@ -1,24 +1,24 @@
 exports.up = function(knex, Promise) {
-	return Promise.all([
-		knex.schema.createTable('gourmets', table => {
-			table.increments('id');
-			table.string('email', 255).notNullable();
-			table.string('firstname', 100).notNullable();
-			table.string('lastname', 100).notNullable();
-			table.json('picture');
-			table.specificType('gender', 'CHAR(1)');
-			table.date('birthdate');
-			table.string('address', 100);
-			table.string('city', 100);
-			table.string('zip_code', 10);
-			table.specificType('location', 'POINT').defaultTo(knex.raw('POINT (48.390394, -4.486076)'));
-			table.text('description');
-			table.timestamps(true, true);
-		}),
-		knex.schema.raw("ALTER TABLE gourmets ADD CONSTRAINT gender_constraint CHECK(gender='F' OR gender='M' OR gender='U');"),
+  return Promise.all([
+    knex.schema.createTable('gourmets', table => {
+      table.increments('id');
+      table.string('email', 255).notNullable();
+      table.string('firstname', 100).notNullable();
+      table.string('lastname', 100).notNullable();
+      table.json('picture');
+      table.specificType('gender', 'CHAR(1)');
+      table.date('birthdate');
+      table.string('address', 100);
+      table.string('city', 100);
+      table.string('zip_code', 10);
+      table.specificType('location', 'POINT').defaultTo(knex.raw('POINT (48.390394, -4.486076)'));
+      table.text('description');
+      table.timestamps(true, true);
+    }),
+    knex.schema.raw("ALTER TABLE gourmets ADD CONSTRAINT gender_constraint CHECK(gender='F' OR gender='M' OR gender='U');"),
   ]);
 };
 
 exports.down = function(knex, Promise) {
-	return knex.schema.dropTable('gourmets');
+  return knex.schema.dropTable('gourmets');
 };
