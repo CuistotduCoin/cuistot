c2029778df86da700681c50a0bcf2ca73e95f639
stephme <stephane@inchbase.com>
2018-07-25 18:08:41 +0200

    Fix mutations and improve queries

--- backend/resolvers/utils.js
+++ backend/resolvers/utils.js
@@ -1,5 +1,5 @@
 import connection from '../knexfile';
-import { formatKnexQueryError } from '../utils/utils';
+import { formatKnexQueryError, cleanKnexQueryArgs, isEmpty } from '../utils/utils';
 
 const knex = require('knex')(connection[process.env.NODE_ENV]); // eslint-disable-line
 
@@ -19,7 +19,8 @@ async function getSingleRow(tableName, id, idField = 'id') {
 
 async function insertObject(tableName, args) {
   try {
-    const query = knex(tableName).insert(args).returning('*');
+    const createArgs = cleanKnexQueryArgs(args);
+    const query = knex(tableName).insert(createArgs).returning('*');
     const result = await query;
     if (result.length) {
       return { data: result[0], message: 'success' };
@@ -33,8 +34,11 @@ async function insertObject(tableName, args) {
 
 async function updateObject(tableName, args, idField = 'id') {
   try {
-    const updateArgs = Object.assign({}, args);
+    const updateArgs = cleanKnexQueryArgs(args);
     delete updateArgs[idField];
+    if (isEmpty(updateArgs)) {
+      return { message: 'not modified' };
+    }
     const query = knex(tableName)
       .where(idField, args[idField])
       .update(updateArgs)
