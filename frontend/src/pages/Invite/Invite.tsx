492d8aa5f4228571be56155398a7db88c787723e
Romain Quellec <romain.quellec@gmail.com>
2018-08-31 16:04:32 +0200

    Invite

--- frontend/src/pages/Invite/Invite.tsx
+++ frontend/src/pages/Invite/Invite.tsx
@@ -1,4 +1,11 @@
 import * as content from "!raw-loader!content/invite.md";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Typography
+} from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import { Theme, withStyles } from "@material-ui/core/styles";
 // @ts-ignore
@@ -9,11 +16,27 @@ import Hero from "components/Hero";
 import React from "react";
 
 const styles = (theme: Theme) => ({
+  card: {
+    width: 300
+  },
+  cardHeader: {
+    background: "rgba(0, 0, 0, 0.4)",
+    color: "fff",
+    height: 40,
+    marginTop: -72
+  },
   grid: {
     margin: "0px auto",
     maxWidth: 1080,
     padding: 24
-  }
+  },
+  link: {
+    textDecoration: "none"
+  },
+  media: {
+    height: 135
+  },
+  title: { color: "white" }
 });
 
 interface IInviteProps {
@@ -40,6 +63,44 @@ export class Invite extends React.Component<IInviteProps, {}> {
           className={classes.grid}
         >
           <MarkdownElement text={content} />
+        </Grid>
+        <Grid
+          container={true}
+          justify="space-around"
+          alignItems="center"
+          spacing={16}
+          className={classes.grid}
+        >
+          <Grid item={true} xs={12}>
+            <Grid container={true} justify="center">
+              <a
+                className={classes.link}
+                href="https://cuistotducoin.typeform.com/to/s9TN8K"
+                target="_blank"
+              >
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.media}
+                    image="https://static.cuistotducoin.com/img/business/buffet.jpg"
+                    title="Invitez vos proches"
+                  />
+                  <CardHeader
+                    className={classes.cardHeader}
+                    title="Invitez vos proches"
+                    classes={{
+                      title: classes.title
+                    }}
+                  />
+                  <CardContent className={classes.cardShortContent}>
+                    <Typography component="p">
+                      Invitez vos proches pour profiter de l'offre spéciale de
+                      septembre !
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </a>
+            </Grid>
+          </Grid>
         </Grid>
         <Footer />
       </>
