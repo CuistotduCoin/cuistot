ad91c017699fa9c3230786d6e8103655db93c29c
stephme <stephane@inchbase.com>
2018-09-04 12:03:27 +0200

    Restore profile page

--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -1,43 +1,23 @@
-import { Theme, withStyles } from "@material-ui/core/styles";
-import { PhotoPicker, S3Image } from "aws-amplify-react";
+import { withStyles } from "@material-ui/core/styles";
 import Footer from "components/Footer";
 import Header from "components/Header";
 import React from "react";
-import { Link } from "react-router-dom";
-import { Storage } from "shared/auth";
 
-const styles = (theme: Theme) => ({});
+const styles = () => ({});
 
-interface IProfileProps {
+interface IProfilProps {
   classes?: any;
 }
 
-export class Profile extends React.Component<IProfileProps, {}> {
-  constructor(props) {
-    super(props);
-    this.state = { url: null };
-  }
-
-  public componentDidMount() {
-    Storage.get("portrait_stephane-d0c6b33c.jpg", { identityId: "xxx" })
-      .then(result => {
-        this.setState({ url: result });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
-
+export class Profil extends React.Component<IProfilProps, {}> {
   public render() {
-    const { classes } = this.props;
-
     return (
       <>
-        <img src={this.state.url} />
-        <S3Image level="protected" picker />
+        <Header />
+        <Footer />
       </>
     );
   }
 }
 
-export default withStyles(styles as any)(Profile as any) as any;
+export default withStyles(styles as any)(Profil as any) as any;
