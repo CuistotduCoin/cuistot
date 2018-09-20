24accb673b4b83f44d4113e077d200a4f28b85cf
stephme <stephane@inchbase.com>
2018-09-20 10:40:13 +0200

    Improve front deployment

--- frontend/loadenv.js
+++ frontend/loadenv.js
@@ -1,20 +1,10 @@
 const fs = require('fs');
 const awsParamEnv = require('aws-param-env'); // eslint-disable-line
-const region = 'eu-west-1';
-const stage = process.env.NODE_ENV;
 
-if (!stage) {
-  throw new Error('Stage is missing !');
-}
-
-awsParamEnv.load(`/cuistot/${stage}`, { region });
-awsParamEnv.load('/cuistot/front', { region });
-awsParamEnv.load(`/cuistot/front/${stage}`, { region });
+awsParamEnv.load('/cuistot', { region: 'eu-west-1' });
 
 const content = `\
-AWS_DOMAIN=${process.env.domain}
 AWS_REGION_IRELAND=eu-west-1
-AWS_DEPLOYMENT_BUCKET=${process.env['deployment-bucket']}
 AWS_USERPOOL_ID=${process.env['userpool-id']}
 AWS_IDENTITY_POOL_ID=${process.env['identity-pool-id']}
 AWS_STORE_BUCKET=${process.env['store-bucket']}
@@ -26,7 +16,6 @@ ALGOLIASEARCH_SEARCH_APP_ID=${process.env['algolia-search-app-id']}
 ALGOLIASEARCH_SEARCH_KEY=${process.env['algolia-search-key']}
 ALGOLIASEARCH_PLACES_APP_ID=${process.env['algolia-places-app-id']}
 ALGOLIASEARCH_PLACES_KEY=${process.env['algolia-places-key']}
-SENTRY_DSN=${process.env['sentry-dsn']}
 `;
 
 fs.writeFile('.env', content, (err) => {
