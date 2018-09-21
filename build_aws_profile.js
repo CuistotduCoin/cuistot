e35f8be28be9637808cae2c99d088b716506ad89
stephme <stephane@inchbase.com>
2018-09-21 10:57:56 +0200

    try

--- build_aws_profile.js
+++ build_aws_profile.js
@@ -17,8 +17,8 @@ fs.writeFile(`${process.env.AWS_CONFIG_PATH}/config`, content, (err) => {
 
 content = `\
 [circle]
-aws_access_key_id = ${process.env.AWS_ACCESS_KEY_ID}
-aws_secret_access_key = ${process.env.AWS_SECRET_ACCESS_KEY}
+aws_access_key_id = ${process.env.AWS_KEY}
+aws_secret_access_key = ${process.env.AWS_SECRET_KEY}
 `;
 
 fs.writeFile(`${process.env.AWS_CONFIG_PATH}/credentials`, content, (err) => {
