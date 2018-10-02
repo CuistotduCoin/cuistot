6ab8e5a324428c1a362ecdff02a56be0d3c23479
Romain Quellec <romain.quellec@gmail.com>
2018-10-02 20:09:14 +0200

    sls

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,6 +1,12 @@
+const path = require('path');
+
 module.exports = {
     entry: './build/lambda.js',
     target: 'node',
+    node: {
+        __dirname: true,
+        __filename: true,
+    },
     module: {
         rules: [
             {
@@ -10,5 +16,13 @@ module.exports = {
                 exclude: /node_modules/
             }
         ]
+    },
+    mode: "production",
+    optimization: {
+        minimize: false
+    },
+    output: {
+        filename: 'handler.js',
+        path: path.resolve(__dirname, 'build')
     }
 };
