25ad3c35bbf14e893e41533e9f480e08a8096242
Romain Quellec <romain.quellec@gmail.com>
2018-11-21 11:13:52 +0100

    algolia search

--- backend/resolvers/workshop-resolver.js
+++ backend/resolvers/workshop-resolver.js
@@ -8,6 +8,10 @@ import {
   performPagination,
   recreateObject,
 } from './utils';
+import algoliasearch from algoliasearch;
+
+var client = algoliasearch(process.env.ALGOLIASEARCH_SEARCH_APP_ID, process.env.ALGOLIASEARCH_SEARCH_KEY);
+var index = client.initIndex(process.env.ALGOLIASEARCH_INDEX);
 
 const TABLE_NAME = 'workshops';
 
@@ -34,9 +38,6 @@ async function getWorkshopBookings(args) {
 
 async function createWorkshop(args) {
   const result = await insertObject(TABLE_NAME, args);
-  if (result) {
-    index.addObject(args);
-  }
   return result;
 }
 
@@ -48,8 +49,8 @@ async function updateWorkshop(args) {
     'cook_id',
     () => getWorkshop({ workshop_id: updateArgs.id }),
   );
-  if (result) {
-    index.saveObject(updateArgs);
+  if (result && result.data && result.data.confirmed == true) {
+    index.saveObject(result.data);
   }
   return result;
 }
@@ -61,22 +62,25 @@ async function deleteWorkshop(args) {
     'cook_id',
     () => getWorkshop({ workshop_id: args.id }),
   );
-  if (result) {
-    index.deleteObject(args.id);
+  if (result && result.data && result.data.confirmed == true) {
+    index.deleteObject(result.data);
   }
   return result;
 }
 
 async function recreateWorkshop(args) {
   const result = await recreateObject(TABLE_NAME, args.id);
-  if (result) {
-    index.addObject(args);
+  if (result && result.data && result.data.confirmed == true) {
+    index.addObject(result.data);
   }
   return result;
 }
 
 async function confirmWorkshop(args) {
   const result = await updateObject(TABLE_NAME, { id: args.id, confirmed: true });
+  if (result && result.data && result.data.confirmed == true) {
+    index.addObject(result.data);
+  }
   return result;
 }
 
